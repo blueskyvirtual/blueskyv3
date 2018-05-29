1c2a075434d25e9762e6ac5136676e455d5443ea
Jonathan P. Voss <jvoss@onvox.net>
2018-05-29 13:17:21 -0500

    module rename and version set

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,9 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Blueskyv3
+module Bluesky
+  VERSION = "3.0.0"
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
